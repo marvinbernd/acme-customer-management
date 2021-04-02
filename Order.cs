5cc8533c4c45dd58b29d296aac965fd69a787ac0
Marvin Bernd <hello@marvinbernd.de>
2021-04-02 22:06:31 +0200

    add Order and OrderItem Class

---
+++ Order.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+            // Code that generates the order date.
+        }
+
+        public int OrderId { get; private set; }
+        public int OrderDate { get; private set; }
+
+        /// <summary>
+        /// Validates the order data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            // Code that validates the order data.
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Retrieves the defined order.
+        /// </summary>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            // Code that retrieves the defined order.
+
+            return new Order();
+        }
+
+        /// <summary>
+        /// Retrieves all orders.
+        /// </summary>
+        /// <returns></returns>
+        public List<Order> Retrieve()
+        {
+            // Code that retrieves all orders.
+
+            return new List<Order>();
+        }
+
+        /// <summary>
+        /// Saves the defined order.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined order.
+
+            return true;
+        }
+    }
+}
---
+++ OrderItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; private set; }
+        public Product OrderProduct { get; set; }
+        public int Quantity { get; set; }
+        public float PurchasePrice { get; set; }
+
+        /// <summary>
+        /// Validates the order item data.
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Retrieves the defined order item.
+        /// </summary>
+        /// <returns></returns>
+        public OrderItem Retrieve(int orderItemId)
+        {
+            // Code that retrieves the defined order item.
+
+            return new OrderItem();
+        }
+
+        /// <summary>
+        /// Retrieves all order items.
+        /// </summary>
+        /// <returns></returns>
+        public List<OrderItem> Retrieve()
+        {
+            // Code that retrieves all order items.
+
+            return new List<OrderItem>();
+        }
+
+        /// <summary>
+        /// Saves the defined order item.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined order item.
+
+            return true;
+        }
+    }
+}
